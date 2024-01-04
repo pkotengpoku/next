f3ca8f5f4902968527c2bf7e7b9019eb5d67ff64
pkotengpoku <135516423+pkotengpoku@users.noreply.github.com>
2024-01-04 08:29:51 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"...me of one of the included themes for dark mode
     styled: false, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
+    utils: false, // adds responsive and modifier utility classes
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
     themeRoot: ":kegrc", // The element that receives theme color CSS variables
